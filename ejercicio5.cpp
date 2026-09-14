bf60a3af5586ee626638ed832521debbc3e8bcc3
maryuriruiz <maryuriruizv08@gmail.com>
2026-09-13 22:52:31 -0500

    agrego el ejercicio 5

---
+++ ejercicio5.cpp
@@ -0,0 +1,29 @@
+/*Ejercicio 4
+Autor: Maryuri Ruiz 
+Fecha: 13 de septiembre
+
+Enunciado:
+    Desarrolla un programa que pida al usuario un número entero positivo n y luego
+    incremente un contador desde 0 hasta n, usando una función que actualice el valor
+    del contador.
+    El objetivo es que practiques el uso de paso por referencia para modificar una
+    variable desde una función.
+    El programa debe mostrar el valor del contador en cada incremento.
+
+E/P/S
+*Entrada:
+    - Ingreso de un numero entero positivo n
+*Proceso:
+    -Incrementar un contador desde 0 hasta n usando una funcion que actualice el valor del contador
+*Salida:
+    -Mostrar el valor del contador en cada incremento
+*/  
+
+#include <iostream>
+using namespace std;
+
+//Prototipo de la funcion
+void incrementarContador(int n, int &contador);
+
+
+}
